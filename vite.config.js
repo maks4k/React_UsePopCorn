8b7d9cfcbaf5860296db8785527cb2501c211d9c
Maksim Shkryabin <maks4k32rus@gmail.com>
2025-03-11 10:37:06 +0300

    продолжаем деплой

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://maks4k.github.io/React_UsePopCorn/',
+  base: '/React_UsePopCorn/',
   server:{
     proxy:{
       "/api":{
